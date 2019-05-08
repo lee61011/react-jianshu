37a38354668e51d3f3d28aad4983146f45f3aa83
lee61011 <lee61011@126.com>
2019-05-08 16:36:26 +0800

    使用 redux-immutable 统一数据格式

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,6 @@
 /* 将拆分出去的数据进行合并 */
 
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux-immutable'
 import { reducer as headerReducer } from '../common/header/store'
 
 const reducer = combineReducers({
