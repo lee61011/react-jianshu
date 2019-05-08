89fadd39e8fe8bae4ce38fd2da2c09c8bf2bb9fc
lee61011 <lee61011@126.com>
2019-05-08 14:01:36 +0800

    使用 combineReducers 对数据进行拆分管理[F

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,18 +1,10 @@
-/* 默认数据 */
-const defaultState = {
-    focused: false
-}
+/* 将拆分出去的数据进行合并 */
 
-export default (state = defaultState, action) => {
-    if (action.type === 'search_focus') {
-        return {
-            focused: true
-        }
-    }
-    if (action.type === 'search_blur') {
-        return {
-            focused: false
-        }
-    }
-    return state;
-}
+import { combineReducers } from 'redux'
+import { reducer as headerReducer } from '../common/header/store'
+
+const reducer = combineReducers({
+    header: headerReducer
+});
+
+export default reducer;
