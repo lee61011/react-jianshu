9b1045921d2ccbcdc6bf6069f77a5a61cd85af8a
lee61011 <lee61011@126.com>
2019-05-09 18:36:43 +0800

    在 React 中使用路由功能

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import store from './store'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './common/header'
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
     <div className="App">
       <Provider store={store}>
         <Header />
+        <BrowserRouter>
+          <div>
+            <Route path='/' exact render={() => <div>home</div>}></Route>
+            <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+          </div>
+        </BrowserRouter>
       </Provider>
     </div>
   );
