f143b06559b3416579dedc6d89b0e66f6502b404
lee61011 <lee61011@126.com>
2019-05-11 12:27:18 +0800

    加载更多

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
-import { ListItem, ListInfo } from '../style'
+import { ListItem, ListInfo, LoadMore } from '../style'
+import { actionCreators } from '../store'
 import { connect } from 'react-redux'
 
 class List extends Component {
     render () {
-        const { list } = this.props
+        const { list, getMoreList, page } = this.props
         return (
             <div>
                 {
-                    list.map((item) => {
+                    list.map((item, index) => {
                         return (
-                            <ListItem key={item.get('id')}>
+                            <ListItem key={index}>
                                 <img className='pic' src={item.get('imgUrl')} alt='' />
                                 <ListInfo>
                                     <h3 className='title'>{item.get('title')}</h3>
@@ -20,12 +21,19 @@ class List extends Component {
                         )
                     })
                 }
+                <LoadMore onClick={() => getMoreList(page)}>加载更多</LoadMore>
             </div>
         )
     }
 }
 
 const mapState = (state) => ({
-    list: state.getIn(['home', 'articleList'])
+    list: state.getIn(['home', 'articleList']),
+    page: state.getIn(['home', 'articlePage'])
 })
-export default connect(mapState)(List)
+const mapDispatch = (dispatch) => ({
+    getMoreList(page) {
+        dispatch(actionCreators.getMoreList(page))
+    }
+})
+export default connect(mapState, mapDispatch)(List)
--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { fromJS } from 'immutable'
 import * as constants from './constants'
 
 const changeHomeData = (result) => ({
@@ -7,12 +8,25 @@ const changeHomeData = (result) => ({
     articleList: result.articleList,
     recommendList: result.recommendList
 })
+const addHomeList = (list, nextPage) => ({
+    type: constants.ADD_ARTICLE_LIST,
+    list: fromJS(list),
+    nextPage
+})
 
 export const getHomeInfo = () => {
     return (dispatch) => {
         axios.get('/api/home.json').then((res) => {
             const result = res.data.data
             dispatch(changeHomeData(result))
+        })
+    }
+}
+export const getMoreList = (page) => {
+    return (dispatch) => {
+        axios.get('/api/homeList.json?page=' + page).then((res) => {
+            const result = res.data.data
+            dispatch(addHomeList(result, page + 1))
         })
     }
 }
