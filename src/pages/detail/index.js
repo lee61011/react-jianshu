2f6c160c1a3de31949581e6d37b8977bbc2738af
lee61011 <lee61011@126.com>
2019-05-12 12:11:44 +0800

    详情页面布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import { DetailWrapper, Header, Content } from './style'
 
 class Detail extends Component {
     render() {
         return (
-            <div>Detail</div>
+            <DetailWrapper>
+                <Header>文章标题</Header>
+                <Content>
+                    <img src='https://upload-images.jianshu.io/upload_images/16557762-b59fc4d5a04a9f37?imageMogr2/auto-orient/' alt='' />
+                    <p><b>文章内容文章内容文章内容文章内容文章内容</b></p>
+                    <p>文章内容文章内容文章内容文章内容文章内容</p>
+                    <p>文章内容文章内容文章内容文章内容文章内容</p>
+                    <p>文章内容文章内容文章内容文章内容文章内容</p>
+                    <p>文章内容文章内容文章内容文章内容文章内容</p>
+                    <p>文章内容文章内容文章内容文章内容文章内容</p>
+                </Content>
+            </DetailWrapper>
         )
     }
 }
