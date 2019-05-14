dd8bbb36ad564f9bbac57eebfb808990289eff2a
lee61011 <lee61011@126.com>
2019-05-14 23:21:27 +0800

    登录页面布局

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Detail from './pages/detail'
+import Login from './pages/login'
 
 import Header from './common/header'
 
@@ -17,6 +18,7 @@ function App() {
             <Header />
             <Route path='/' exact component={Home}></Route>
             <Route path='/detail/:id' exact component={Detail}></Route>
+            <Route path='/login' exact component={Login}></Route>
           </div>
         </BrowserRouter>
       </Provider>
