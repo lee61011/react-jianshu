da42fa37b4f0c54419977df48f0788e85e3c4d4f
lee61011 <lee61011@126.com>
2019-05-15 23:46:26 +0800

    登录功能实现

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -3,6 +3,7 @@ import { CSSTransition } from 'react-transition-group'
 import { connect } from 'react-redux'
 import { actionCreators } from './store'
 import { Link } from 'react-router-dom'
+import { actionCreators as loginActionCreators } from '../../pages/login/store'
 
 import { HeaderWrapper, Logo, Nav, NavItem, SearchWrapper, NavSearch, SearchInfo, SearchInfoTitle, SearchInfoSwitch, SearchInfoList, SearchInfoItem, Addition, Button } from './style'
 
@@ -45,7 +46,7 @@ class Header extends Component {
     }
 
     render() {
-        const { focused, list, handleInputFocus, handleInputBlur } = this.props
+        const { focused, list, login, logout, handleInputFocus, handleInputBlur } = this.props
         return (
             <HeaderWrapper>
                 <Link to='/'>
@@ -54,7 +55,9 @@ class Header extends Component {
                 <Nav>
                     <NavItem className="left active">首页</NavItem>
                     <NavItem className="left">下载App</NavItem>
-                    <NavItem className="right">登录</NavItem>
+                    {
+                        login ? <NavItem onClick={logout} className="right">退出</NavItem> : <Link to='/login'><NavItem className="right">登录</NavItem></Link>
+                    }
                     <NavItem className="right">
                         <i className="iconfont">&#xe601;</i>
                     </NavItem>
@@ -94,7 +97,8 @@ const mapStateToProps = (state) => {
         list: state.getIn(['header', 'list']),
         page: state.getIn(['header', 'page']),
         totalPage: state.getIn(['header', 'totalPage']),
-        mouseIn: state.getIn(['header', 'mouseIn'])
+        mouseIn: state.getIn(['header', 'mouseIn']),
+        login: state.getIn(['login', 'login'])
     }
 }
 
@@ -130,9 +134,11 @@ const mapDispathToProps = (dispatch) => {
             } else {
                 dispatch(actionCreators.changePage(1))
             }
+        },
+        logout(){
+            dispatch(loginActionCreators.logout())
         }
     }
 }
-
 
 export default connect(mapStateToProps, mapDispathToProps)(Header);
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,26 +1,35 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { actionCreators } from './store'
 import { LoginWrapper, LoginBox, Input, Button } from './style'
 
 class Login extends PureComponent {
     render() {
-        return (
-            <LoginWrapper>
-                <LoginBox>
-                    <Input placeholder='账号' />
-                    <Input placeholder='密码' />
-                    <Button>登录</Button>
-                </LoginBox>
-            </LoginWrapper>
-        )
+        const { loginStatus, account, password } = this.props
+        if (!loginStatus) {
+            return (
+                <LoginWrapper>
+                    <LoginBox>
+                        <Input innerRef={ (input) => {this.account = input} } placeholder='账号' />
+                        <Input type='password' innerRef={ (input) => {this.password = input}} placeholder='密码' />
+                        <Button onClick={ () => this.props.login(this.account, this.password) }>登录</Button>
+                    </LoginBox>
+                </LoginWrapper>
+            )
+        } else {
+            return <Redirect to='/' />
+        }
     }
 }
 
 const mapState = (state) => ({
-
+    loginStatus: state.getIn(['login', 'login'])
 })
 const mapDispatch = (dispatch) => ({
-
+    login(accountElem, passwordElem) {
+        dispatch(actionCreators.login(accountElem.value, passwordElem.value))
+    }
 })
 
-export default connect(mapState, null)(Login)
+export default connect(mapState, mapDispatch)(Login)
